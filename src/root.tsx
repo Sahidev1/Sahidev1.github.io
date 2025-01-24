974df1c5bb970cb3e25876289425001611e9bdd4
Sahidev1 <theoddrogue@gmail.com>
2025-01-24 15:54:55 +0100

    savepoint

--- src/root.tsx
+++ src/root.tsx
@@ -3,21 +3,26 @@ import React from 'react';
 import { Container, createRoot, Root } from 'react-dom/client';
 import './styles.css';
 import App from './App';
-import { HashRouter as Router, Route, Switch as Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router';
 import Topbar from './components/Topbar';
 import MainPage from './pages/MainPage';
 import AboutPage from './pages/AboutPage';
 
 
+
+
+
 const rootReference: Container = document.getElementById('root') as HTMLElement;
 const root:Root = createRoot(rootReference);
 root.render(
-    <Router>
+    <BrowserRouter>
         <Routes>
             <Route path='/' element={<App/>}>
                 <Route index={true} element={<MainPage/>}/>
                 <Route path='about' element={<AboutPage/>}/>
             </Route>
         </Routes>
-    </Router>
+    </BrowserRouter>
 );
+
+
