1c34374207aaf93c3e3c7f03b1eae8e40e4b09b5
Sahidev1 <theoddrogue@gmail.com>
2025-01-28 18:25:07 +0100

    scss file implemented

--- src/root.tsx
+++ src/root.tsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import { Container, createRoot, Root } from 'react-dom/client';
-import './styles.css';
+import './styles.scss';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import Topbar from './components/Topbar';
--- webpack.config.js
+++ webpack.config.js
@@ -23,8 +23,8 @@ module.exports = (env) => {
           exclude: /node_modules/,
         },
         {
-          test: /\.css$/,
-          use: ['style-loader', 'css-loader'],
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
         },
       ],
     },
