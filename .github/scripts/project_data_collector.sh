4a15e3e431ead6f23117b9c3dedea6573f71ac00
Sahidev1 <theoddrogue@gmail.com>
2025-02-07 17:39:37 +0100

    further improved project data collection script

--- .github/scripts/project_data_collector.sh
+++ .github/scripts/project_data_collector.sh
@@ -8,16 +8,17 @@ echo "$MODE"
 
 GITHUB_API_LINK="https://api.github.com/repos/Sahidev1/"
 GITHUB_USERS_API="https://api.github.com/users/Sahidev1/"
-
+GITHUB_RAW_API="https://raw.githubusercontent.com/Sahidev1/"
 
 projects=($(curl -s "${GITHUB_USERS_API}repos" | jq -r '.[].name'))
 
-
+PROJECT_DATA_PATH=".personalproject/data.json"
 
 
 
 
 DATA_PREFIX="project_data_"
+PROJECT_DATA_INFIX="detailed_"
 FILE_EXT=".json"
 
 
@@ -32,6 +33,20 @@ fi
 
 
 for project in "${projects[@]}"; do
-    curl -H "Accept: application/json" -X GET "${GITHUB_API_LINK}${project}" -o "${RELATIVE_FILE_PATH}${DATA_PREFIX}${project}${FILE_EXT}"
-    #echo "${GITHUB_API_LINK}${project}, " "${RELATIVE_FILE_PATH}${DATA_PREFIX}${project}${FILE_EXT}"
+    # Get repo data
+    repo_response=$(curl -s -H "Accept: application/json" -X GET "${GITHUB_API_LINK}${project}")
+    
+    # Check if topics include "personal-project"
+    if echo "$repo_response" | jq -e '.topics | contains(["personal-project"])' > /dev/null; then
+        default_branch=$(echo "$repo_response" | jq -r '.default_branch')
+        
+        # Get personal project data
+        project_data=$(curl -s "${GITHUB_RAW_API}${project}/${default_branch}/${PROJECT_DATA_PATH}")
+        
+        # Combine repo data with personal project data
+        repo_response=$(echo "$repo_response" | jq --argjson proj "$project_data" '. + {"personal-project": $proj}')
+    fi
+    
+    # Save combined data
+    echo "$repo_response" > "${RELATIVE_FILE_PATH}${DATA_PREFIX}${project}${FILE_EXT}"
 done
