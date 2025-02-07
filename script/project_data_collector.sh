18480d0e70259c4e6a79931132824d9edc42149d
Sahidev1 <theoddrogue@gmail.com>
2025-02-07 14:00:58 +0100

    Updates

--- script/project_data_collector.sh
+++ script/project_data_collector.sh
@@ -11,13 +11,16 @@ projects=('myVM' 'recruitmentapp' 'calculator')
 DATA_PREFIX="project_data_"
 FILE_EXT=".json"
 
-RELATIVE_FILE_PATH="../cache/"
-
-mkdir "../cache" 
 
 if [[ "${MODE}" == "test" ]]; then
     RELATIVE_FILE_PATH=""
-fi
+elif [[ "${MODE}" == "dev" ]]; then
+    RELATIVE_FILE_PATH="../../public/backup_cache/"
+else
+    RELATIVE_FILE_PATH="../cache/"
+    mkdir "../cache" 
+fi 
+
 
 for project in "${projects[@]}"; do
     curl -H "Accept: application/json" -X GET "${GITHUB_API_LINK}${project}" -o "${RELATIVE_FILE_PATH}${DATA_PREFIX}${project}${FILE_EXT}"
